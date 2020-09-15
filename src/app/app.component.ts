c1b20aa917289b41f1191e183c99fbd6e669f17b
krishna <krishna@durity.life>
2020-09-15 22:38:18 +0530

    added docker file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'grokonez.com';
-  description = 'Angular-Node.js Demo';
+  title = 'krishna demo';
+  description = 'uploading a file';
 }
