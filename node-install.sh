262b69f68c2e497aed34b40fcade79f04444949b
krrish8055 <52241501+krrish8055@users.noreply.github.com>
2024-07-31 16:38:25 +0530

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -4,7 +4,7 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo node --version
 sudo npm --version
-sudo npm install -g @angular/cli
+sudo npm install -g @angular/cli@6.0.3
 sudo ng --version
 sudo npm cache clean -f
 sudo npm install -g n
